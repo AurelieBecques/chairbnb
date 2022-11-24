ae4522ebffb970621a46702559ec1a7224d4e925
Aurelie Becques <aurelie.becques@gmail.com>
2022-11-24 13:06:28 +0100

    add-validation

--- app/controllers/owner_bookings_controller.rb
+++ app/controllers/owner_bookings_controller.rb
@@ -1,5 +1,21 @@
 class OwnerBookingsController < ApplicationController
   def index
-    @bookings = current_user.owner_bookings
+    @bookings = current_user.owner_bookings.order(created_at: :desc)
+  end
+
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.status = "accepté"
+    @booking.save
+
+    redirect_to owner_bookings(current_user)
+  end
+
+  def deny
+    @booking = Booking.find(params[:id])
+    @booking.status = "refusé"
+    @booking.save
+
+    redirect_to owner_bookings(current_user)
   end
 end
