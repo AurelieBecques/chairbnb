ce7be6d122b6e80dca2558ddff459402125a210b
Rodolphe Junker <rody@ancre-agence.com>
2022-11-25 12:08:21 +0100

    Add map

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -2,6 +2,14 @@ class ChairsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
     @chairs = Chair.all
+    @markers = @chairs.geocoded.map do |chair|
+      {
+        lat: chair.latitude,
+        lng: chair.longitude,
+        info_window: render_to_string(partial: "info_window", locals: {chair: chair}),
+        image_url: helpers.asset_url("map-pin-solid.svg")
+      }
+    end
   end
 
   def show
--- app/models/chair.rb
+++ app/models/chair.rb
@@ -3,5 +3,6 @@ class Chair < ApplicationRecord
   has_many :bookings
   validates :name, :price_per_day, presence: true
   has_one_attached :photo
-
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
